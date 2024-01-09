c093804b726a0e9e6e4fdce5bf2ecb5f74876506
allanteone <allanteone@gmail.com>
2024-01-09 22:50:08 +0200

    cw-47

--- src/telran/multithreading/myblockingqueue/messaging/MyLinkedBlockingQueue.java
+++ src/telran/multithreading/myblockingqueue/messaging/MyLinkedBlockingQueue.java
@@ -1,70 +1,107 @@
 package telran.multithreading.myblockingqueue.messaging;
 
+import java.util.LinkedList;
 import java.util.concurrent.TimeUnit;
 
 public class MyLinkedBlockingQueue<E> implements MyBlockingQueue<E> {
-    //TODO fields
+	private  LinkedList<E> queue;
+    private int capacity;
+
 	public MyLinkedBlockingQueue (int capacity) {
-		//TODO
+		this.capacity = capacity;
+		this.queue = new LinkedList<>();
 	}
 	@Override
-	public boolean add(E obj) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean offer(E obj) {
-		// TODO Auto-generated method stub
-		return false;
+	public synchronized boolean add(E obj) {
+		if (this.queue.size() >= capacity) {
+			throw new IllegalStateException();
+		}
+		queue.push(obj);
+		return true;
 	}
 
 	@Override
-	public void put(E obj) throws InterruptedException {
-		// TODO Auto-generated method stub
+	public synchronized boolean offer(E obj) {
+		boolean res = true;
 
+		try {
+			this.add(obj);
+		} catch (IllegalStateException e) {
+			res = false;
+		}
+		return res;
 	}
 
 	@Override
-	public boolean offer(E obj, long timeout, TimeUnit unit) throws InterruptedException {
-		// TODO Auto-generated method stub
-		return false;
+	public synchronized void put(E obj) throws InterruptedException {
+		while (queue.size() >= capacity) {
+			wait();
+		}
+		this.add(obj);
+		notifyAll();
 	}
 
 	@Override
-	public E remove() {
-		// TODO Auto-generated method stub
-		return null;
+	public synchronized boolean offer(E obj, long timeout, TimeUnit unit) throws InterruptedException {
+		var startTime = System.currentTimeMillis();
+		var timeRunning = unit.toMillis(timeout);
+
+		while (queue.size() >= capacity && System.currentTimeMillis() <= startTime + timeRunning) {
+			wait();
+		}
+		return this.offer(obj);
 	}
 
 	@Override
-	public E poll() {
-		// TODO Auto-generated method stub
-		return null;
+	public synchronized E remove() {
+		return queue.pop();
 	}
 
 	@Override
-	public E take() throws InterruptedException {
-		// TODO Auto-generated method stub
-		return null;
+	public synchronized E poll() {
+		E res = null;
+
+		try {
+			res = this.remove();
+		} catch (Exception e) {
+		}
+		return res;
 	}
 
 	@Override
-	public E poll(long timeout, TimeUnit unit) {
-		// TODO Auto-generated method stub
-		return null;
+	public synchronized E take() throws InterruptedException {
+		while (this.queue.size() == 0) {
+			wait();
+		}
+		return this.poll();
 	}
 
 	@Override
-	public E element() {
-		// TODO Auto-generated method stub
-		return null;
+	public synchronized E poll(long timeout, TimeUnit unit) throws InterruptedException {
+		var startTime = System.currentTimeMillis();
+		var timeRunning = unit.toMillis(timeout);
+
+		while (queue.size() == 0 && System.currentTimeMillis() <= startTime + timeRunning) {
+			wait();
+		}
+		return this.poll();
 	}
 
 	@Override
-	public E peek() {
-		// TODO Auto-generated method stub
-		return null;
+	public synchronized E element() {
+		return this.queue.element();
+	}
+
+	@Override
+	public synchronized E peek() {
+		E res = null;
+
+		try {
+			res = this.element();
+		} catch (Exception e) {
+
+		}
+		return res;
 	}
 
 }
